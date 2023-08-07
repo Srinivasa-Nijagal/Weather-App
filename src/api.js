6de481b0b2c1a79a2ea923500eb54d7b46986835
Srinivasa-Nijagal <srinivasa.nijagal2@gmail.com>
2023-08-07 09:30:34 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd0e27448a8mshbf2dfd8594e53e4p1838ecjsn00eea249a3c4',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8372cb1b0e9b34740000f784240f8f6d";
